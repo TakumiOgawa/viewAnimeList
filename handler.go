12496c96a12fbbbba1b1554f8c97b14d74064c15
小川拓海 <takumi_ogawa@ogawatakuminoMacBook-Pro.local>
2019-09-17 02:24:19 +0900

    [fix]slack機構を実装

--- handler.go
+++ handler.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"errors"
 	"log"
-	"net/url"
+	"os"
 	"strings"
 
 	"github.com/TakumiOgawa/viewAnimeList/scraper"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/nlopes/slack"
+	"github.com/nlopes/slack/slackevents"
 )
 
 var (
@@ -27,48 +29,58 @@ func main() {
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	var tw string
-	var un string
-	for _, value := range strings.Split(request.Body, "&") {
-		param := strings.Split(value, "=")
-		if param[0] == "trigger_word" {
-			tw, _ = url.QueryUnescape(param[1])
+	log.Println("method:", request.HTTPMethod, "path:", request.Path, "res:", request.Resource)
+	eventsAPIEvent, _ := slackevents.ParseEvent(json.RawMessage(request.Body),
+		slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: "TOKEN"}))
+
+	if eventsAPIEvent.Type == slackevents.URLVerification {
+		var r *slackevents.ChallengeResponse
+
+		return events.APIGatewayProxyResponse{
+			Body: r.Challenge,
+			Headers: map[string]string{
+				"Content-Type": "text"},
+			StatusCode: 200,
+		}, nil
+
+		// こっからレスポンス
+		api := slack.New(os.Getenv("SLACK_TOKEN"))
+		var season string
+
+		if eventsAPIEvent.Type == slackevents.CallbackEvent {
+			innerEvent := eventsAPIEvent.InnerEvent
+			switch ev := innerEvent.Data.(type) {
+			case *slackevents.AppMentionEvent: // Botユーザーへのメンションの場合
+				reply := "応答テキスト"
+				api.PostMessage(ev.Channel, slack.MsgOptionText(reply, false))
+			case *slackevents.MessageEvent:
+				if ev.ChannelType == "im" { // ダイレクトメッセージの場合
+					text := ev.Text
+					switch {
+					case strings.Contains(text, "春"):
+						season = "spring"
+					case strings.Contains(text, "夏"):
+						season = "summer"
+					case strings.Contains(text, "秋"):
+						season = "autonum"
+					case strings.Contains(text, "冬"):
+						season = "winter"
+					}
+
+					animeList := scraper.GetAnimeLink("2018", season)
+					var outputAnime string
+					for _, anime := range animeList {
+						outputAnime = outputAnime + anime + "\n"
+					}
+
+					api.PostMessage("TARGET_CHANNEL_ID", slack.MsgOptionText(outputAnime, false))
+
+				}
+			}
 		}
-		if param[0] == "user_name" {
-			un, _ = url.QueryUnescape(param[1])
-		}
 	}
-
-	if un == "slackbot" {
-		return events.APIGatewayProxyResponse{}, nil
-	}
-
-	var season string
-	if tw == "春" || tw == "はる" {
-		season = "spring"
-	} else if tw == "夏" || tw == "なつ" {
-		season = "summer"
-	} else if tw == "秋" || tw == "あき" {
-		season = "autumn"
-	} else if tw == "冬" || tw == "ふゆ" {
-		season = "winter"
-	}
-
-	animeList := scraper.GetAnimeLink("2018", season)
-	var outputAnime string
-	for _, anime := range animeList {
-		outputAnime = outputAnime + anime + "\n"
-	}
-
-	j, err := json.Marshal(SlackMessage{Text: outputAnime})
-	if err != nil {
-		log.Print(err)
-		return events.APIGatewayProxyResponse{Body: "エラー"}, err
-	}
-
 	return events.APIGatewayProxyResponse{
-		Body:       string(j),
-		StatusCode: 200,
+		Body:       "badrequest",
+		StatusCode: 400,
 	}, nil
-
 }

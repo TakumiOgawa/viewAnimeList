ad7a57b5a373cf5b32ac5f5ccd61ef30bdf44d13
小川拓海 <takumi_ogawa@ogawatakuminoMacBook-Pro.local>
2019-09-20 02:13:27 +0900

    [WIP]メンションを認識しない＋メンションなしが無限ループする

--- handler.go
+++ handler.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/TakumiOgawa/viewAnimeList/scraper"
 	"github.com/aws/aws-lambda-go/events"
@@ -18,10 +21,15 @@ func main() {
 	lambda.Start(handler)
 }
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	eventsAPIEvent, _ := slackevents.ParseEvent(json.RawMessage(request.Body),
-		slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: os.Getenv("V_TOKEN")}))
+	requestBody := json.RawMessage(request.Body)
+	slackeventsOption := slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: os.Getenv("V_TOKEN")})
+	eventsAPIEvent, err := slackevents.ParseEvent(requestBody, slackeventsOption)
+
+	if err != nil {
+		log.Printf("%s err \n ", err)
+	}
 
 	if eventsAPIEvent.Type == slackevents.URLVerification {
 		var r *slackevents.ChallengeResponse
@@ -39,44 +47,49 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	if eventsAPIEvent.Type == slackevents.CallbackEvent {
 		api := slack.New(os.Getenv("BOT_USER_TOKEN"))
+		var year string
 		var season string
+
+		// デフォルトは現在年
+		year = strconv.Itoa(time.Now().Year())
+
 		innerEvent := eventsAPIEvent.InnerEvent
 		switch ev := innerEvent.Data.(type) {
 		case *slackevents.AppMentionEvent: // Botユーザーへのメンションの場合
-			reply := "応答テキスト"
-			api.PostMessage(ev.Channel, slack.MsgOptionText(reply, false))
-		case *slackevents.MessageEvent:
 			text := ev.Text
-			if text == "春" || text == "夏" || text == "秋" || text == "冬" {
 
-				switch {
-				case strings.Contains(text, "春"):
-					season = "spring"
-				case strings.Contains(text, "夏"):
-					season = "summer"
-				case strings.Contains(text, "秋"):
-					season = "autonum"
-				case strings.Contains(text, "冬"):
-					season = "winter"
-				}
-
-				animeList := scraper.GetAnimeLink("2018", season)
-				var outputAnime string
-				for _, anime := range animeList {
-					outputAnime = outputAnime + anime + "\n"
-				}
-
-				channelID, timestamp, err := api.PostMessage(os.Getenv("TARGET_CHANNEL_ID"), slack.MsgOptionText(outputAnime, false))
-				if err != nil {
-					fmt.Printf("%s\n", err)
-
-				}
-
-				fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+			switch {
+			case strings.Contains(text, "春"):
+				season = "spring"
+			case strings.Contains(text, "夏"):
+				season = "summer"
+			case strings.Contains(text, "秋"):
+				season = "autonum"
+			case strings.Contains(text, "冬"):
+				season = "winter"
 			}
 
+			animeList := scraper.GetAnimeLink(year, season)
+			for _, anime := range animeList {
+				_, _, err := api.PostMessage(os.Getenv("TARGET_CHANNEL_ID"), slack.MsgOptionText(anime, false))
+				if err != nil {
+					log.Printf("ERROR : %s", err)
+
+				}
+			}
+		case *slackevents.MessageEvent:
+			text := ev.Text
+			if !strings.Contains(text, "メンションつけろやオラ") {
+				reply := "メンションつけろやオラ"
+				_, _, err := api.PostMessage(os.Getenv("TARGET_CHANNEL_ID"), slack.MsgOptionText(reply, false))
+				if err != nil {
+					log.Printf("ERROR : %s", err)
+
+				}
+			}
 		}
 	}
+
 	return events.APIGatewayProxyResponse{
 		Body: "失敗",
 		Headers: map[string]string{
